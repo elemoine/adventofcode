214ad84288edd08bfa6d6aaab40106eb9337273e
Éric Lemoine <eric.lemoine@oslandia.com>
2019-12-27 23:19:02 +0100

    Add 2019/day21

---
+++ 2019/day21/part1.py
@@ -0,0 +1,166 @@
+def program(intcode):
+    intcode += [0] * 2000
+    relativebase = 0
+    pos = 0
+    while True:
+        if pos == len(intcode):
+            raise Error("error: reached end of intcode")
+        instruction = str(intcode[pos])
+        instruction = instruction.rjust(5, "0")
+        assert len(instruction) == 5
+        opcode = instruction[-2:]
+        assert len(opcode) == 2
+        if opcode == "99":
+            return intcode
+        if opcode == "01":
+            # addition
+            mode1, mode2, mode3 = modes(instruction)
+            idx1 = modetoindex(mode1, intcode, pos + 1, relativebase)
+            idx2 = modetoindex(mode2, intcode, pos + 2, relativebase)
+            idx3 = modetoindex(mode3, intcode, pos + 3, relativebase)
+            intcode[idx3] = intcode[idx1] + intcode[idx2]
+            pos += 4
+        elif opcode == "02":
+            # multiplication
+            mode1, mode2, mode3 = modes(instruction)
+            idx1 = modetoindex(mode1, intcode, pos + 1, relativebase)
+            idx2 = modetoindex(mode2, intcode, pos + 2, relativebase)
+            idx3 = modetoindex(mode3, intcode, pos + 3, relativebase)
+            intcode[idx3] = intcode[idx1] * intcode[idx2]
+            pos += 4
+        elif opcode == "03":
+            # input
+            mode1, _, _ = modes(instruction)
+            assert mode1 in ("0", "2")
+            idx1 = modetoindex(mode1, intcode, pos + 1, relativebase)
+            intcode[idx1] = yield
+            pos += 2
+        elif opcode == "04":
+            # output
+            mode1, _, _ = modes(instruction)
+            idx1 = modetoindex(mode1, intcode, pos + 1, relativebase)
+            yield intcode[idx1]
+            pos += 2
+        elif opcode == "05":
+            # jump-if-true
+            mode1, mode2, _ = modes(instruction)
+            idx1 = modetoindex(mode1, intcode, pos + 1, relativebase)
+            idx2 = modetoindex(mode2, intcode, pos + 2, relativebase)
+            if intcode[idx1] != 0:
+                pos = intcode[idx2]
+            else:
+                pos += 3
+        elif opcode == "06":
+            # jump-if-false
+            mode1, mode2, _ = modes(instruction)
+            idx1 = modetoindex(mode1, intcode, pos + 1, relativebase)
+            idx2 = modetoindex(mode2, intcode, pos + 2, relativebase)
+            if intcode[idx1] == 0:
+                pos = intcode[idx2]
+            else:
+                pos += 3
+        elif opcode == "07":
+            # less than
+            mode1, mode2, mode3 = modes(instruction)
+            idx1 = modetoindex(mode1, intcode, pos + 1, relativebase)
+            idx2 = modetoindex(mode2, intcode, pos + 2, relativebase)
+            idx3 = modetoindex(mode3, intcode, pos + 3, relativebase)
+            if intcode[idx1] < intcode[idx2]:
+                intcode[idx3] = 1
+            else:
+                intcode[idx3] = 0
+            pos += 4
+        elif opcode == "08":
+            # equals
+            mode1, mode2, mode3 = modes(instruction)
+            assert mode3 in ("0", "2")
+            idx1 = modetoindex(mode1, intcode, pos + 1, relativebase)
+            idx2 = modetoindex(mode2, intcode, pos + 2, relativebase)
+            idx3 = modetoindex(mode3, intcode, pos + 3, relativebase)
+            if intcode[idx1] == intcode[idx2]:
+                intcode[idx3] = 1
+            else:
+                intcode[idx3] = 0
+            pos += 4
+        elif opcode == "09":
+            # adjust the relative base
+            mode1, _, _ = modes(instruction)
+            idx1 = modetoindex(mode1, intcode, pos + 1, relativebase)
+            relativebase += intcode[idx1]
+            pos += 2
+        else:
+            raise Error("error: unknown opcode", opcode)
+
+
+def modes(instruction):
+    mode1 = instruction[-3:-2]
+    assert mode1 in ("0", "1", "2")
+    mode2 = instruction[-4:-3]
+    assert mode1 in ("0", "1", "2")
+    mode3 = instruction[-5:-4]
+    assert mode1 in ("0", "1", "2")
+    return mode1, mode2, mode3
+
+
+def modetoindex(mode, intcode, pos, relativebase):
+    if mode == "0":
+        return intcode[pos]
+    elif mode == "1":
+        return pos
+    elif mode == "2":
+        return relativebase + intcode[pos]
+    raise Error("error: unknown mode")
+
+
+def intcodetolist(intcode):
+    return list(map(int, intcode.strip().split(",")))
+
+
+def _output(prog, c=None):
+    damage = None
+    s = [chr(c)] if c else []
+    while True:
+        try:
+            c = next(prog)
+        except StopIteration:
+            break
+        if c is None:
+            break
+        try:
+            c = chr(c)
+        except ValueError:
+            damage = c
+            break
+        else:
+            s.append(c)
+    print("".join(s))
+    if damage:
+        print("Amount of hull damage:", damage)
+
+
+def _input(prog, i):
+    print(i)
+    for c in i:
+        c = prog.send(ord(c))
+        assert c is None
+    c = prog.send(ord("\n"))
+    return c
+
+
+
+if __name__ == "__main__":
+    with open("input") as f:
+        intcode = f.read()
+    intcode = intcodetolist(intcode)
+    prog = program(intcode)
+
+    _output(prog)
+    c = _input(prog, "NOT A J")
+    c = _input(prog, "NOT B T")
+    c = _input(prog, "AND C T")
+    c = _input(prog, "OR T J")
+    c = _input(prog, "NOT C T")
+    c = _input(prog, "AND D T")
+    c = _input(prog, "OR T J")
+    c = _input(prog, "WALK")
+    _output(prog, c)
