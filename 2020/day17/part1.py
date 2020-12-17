b2db81b3f561359f6f35f16959dde260e1883c50
Éric Lemoine <eric.lemoine@ukg.com>
2020-12-17 10:41:19 +0100

    2020/day17 part1 generalization

--- 2020/day17/part1.py
+++ 2020/day17/part1.py
@@ -1,11 +1,14 @@
-def initial_state(inputfile):
+import itertools
+
+
+def initial_state(inputfile, dim):
     state = set()
     with open(inputfile) as f:
         for x, line in enumerate(f):
             line = line.strip()
             for y, value in enumerate(line):
                 if value == "#":
-                    state.add((x, y, 0))
+                    state.add((x, y) + (0,) * (dim - 2))
     return state
 
 
@@ -13,17 +16,16 @@ def active_cubes(state):
     yield from state
 
 
-def neighbors(cube):
-    x, y, z = cube
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            for k in range(-1, 2):
-                if i != 0 or j != 0 or k != 0:
-                    yield (x + i, y + j, z + k)
+def neighbors(cube, dim):
+    yield from (
+        tuple(map(sum, zip(cube, d)))
+        for d in itertools.product(range(-1, 2), repeat=dim)
+        if d != (0,) * dim
+    )
 
 
-def active_neighbors(cube, state):
-    yield from (c for c in neighbors(cube) if c in state)
+def active_neighbors(cube, state, dim):
+    yield from (c for c in neighbors(cube, dim) if c in state)
 
 
 def count(iter):
@@ -31,14 +33,15 @@ def count(iter):
 
 
 def main():
-    state = initial_state("input")
+    dim = 3
+    state = initial_state("input", dim)
     for cycle in range(1, 7):
         newstate = set(state)
         for cube in active_cubes(state):
-            if count(active_neighbors(cube, state)) not in (2, 3):
+            if count(active_neighbors(cube, state, dim)) not in (2, 3):
                 newstate.remove(cube)
-            for neighbor in neighbors(cube):
-                if count(active_neighbors(neighbor, state)) == 3:
+            for neighbor in neighbors(cube, dim):
+                if count(active_neighbors(neighbor, state, dim)) == 3:
                     newstate.add(neighbor)
         state = newstate
     print(count(active_cubes(state)))
